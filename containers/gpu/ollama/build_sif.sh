fccede9bbad9ede0b414451a71a346329d75b625
Ritchie Ng <ritchieng@u.nus.edu>
2024-02-11 16:29:15 +0800

    Remove ollama test

--- containers/gpu/ollama/build_sif.sh
+++ containers/gpu/ollama/build_sif.sh
@@ -4,7 +4,4 @@ IMAGE_NAME=./singularity_container_${VER}.sif
 # Build
 sudo singularity build ${IMAGE_NAME} ./singularity_container_${VER}.def
 
-# Test pytorch
-singularity exec ${IMAGE_NAME} python -c "import torch;print('pytorch version: ' + torch.__version__)"
-
 echo image: ${IMAGE_NAME}
